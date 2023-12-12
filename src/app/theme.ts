9ca5a33865036cc9fa1e9f58dd0610d3f212b797
pitpat.eth <patrik@nfteam.eu>
2023-12-12 21:49:22 +0100

    Chakra custom theme (font)

---
+++ src/app/theme.ts
@@ -0,0 +1,13 @@
+import { Inter } from "next/font/google";
+import { extendTheme } from "@chakra-ui/react";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const customTheme = extendTheme({
+  fonts: {
+    heading: inter.style.fontFamily,
+    body: inter.style.fontFamily,
+  },
+});
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,8 @@
 "use client";
 import type { ReactNode } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { customTheme } from "../app/theme";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return <ChakraProvider theme={customTheme}>{children}</ChakraProvider>;
 }
