cec7ee7738dba250a72fe8ea89b757ecbf6e50ad
pitpat.eth <patrik@nfteam.eu>
2023-12-12 21:49:05 +0100

    Colors

---
+++ src/utils/Colors.ts
@@ -0,0 +1,9 @@
+export const StargateColors = {
+  primary: "#e340bf",
+  secondary: "#a440e3",
+  grey: "#656f7e",
+  black: "#09090b",
+  darkBg: "#262626",
+  white: "#FFFFFF",
+  lightBg: "#d9d9d9",
+};
