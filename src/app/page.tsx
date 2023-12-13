280a535bfbea8f03443232f62bb23a11ed18ee65
pitpat.eth <kisbalazs.patrik@icloud.com>
2023-12-14 00:00:26 +0100

    text-shadows, use components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Header from "../components/Header";
+import Features from "../sections/features";
+import Functions from "../sections/functions";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <div id="features">
-        Hello Stargate
-      </div>
+      <Features />
+      <Functions />
     </main>
   );
 }
