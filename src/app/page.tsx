bade2df8b753f56602ddc606c1397ca9f0307e73
pitpat.eth <patrik@nfteam.eu>
2023-12-15 01:11:46 +0100

    sections => components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Header from "../components/Header";
-import Audience from "../sections/Audience";
-import Features from "../sections/Features";
-import Functions from "../sections/Functions";
-import Product from "../sections/Product";
+import Audience from "../components/Audience";
+import Features from "../components/Features";
+import Functions from "../components/Functions";
+import Product from "../components/Product";
 
 export default function Home() {
   return (
