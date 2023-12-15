d311a69b892bb6e0644489ec80033fe0a5d45382
pitpat.eth <patrik@nfteam.eu>
2023-12-15 14:20:31 +0100

    scrollbar, motion fix

--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -181,7 +181,7 @@ const Pricing = () => {
               initial={{ opacity: 0.2 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0.2 }}
-              transition={{ duration: 0.5 }}
+              transition={"all 0.25s ease"}
             >
               <Text
                 as="h3"
